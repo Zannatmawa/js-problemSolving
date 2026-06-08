e56b97ea7248e74615a8ee8b36d6f3f922aef40b
zannatul mawa <zannatulmawa38@gmail.com>
2026-06-08 16:31:36 +0600

    solved the problem of finding the largest num

--- day1/largestNum.js
+++ day1/largestNum.js
@@ -4,6 +4,18 @@
 // Input: 3, 7, 5  → Output: 7
 // Hint: Use Math.max() or if-else conditions.
 
-function largestNum() {
-
+function largestNum(a = 0, b = 0, c = 0) {
+    if (a > b) {
+        if (a > c) {
+            console.log("a is the largest number")
+        }
+        else {
+            console.log("c is the largest number")
+        }
+    }
+    else {
+        console.log("b is the largest number")
+    }
 }
+
+largestNum(9, 7, 11)
