8e6f0b368cb425829dec342ee1f4dd1a13b50edd
zannatul mawa <zannatulmawa38@gmail.com>
2026-06-11 14:23:27 +0600

    created folder for day3

---
+++ day3/duplicateFromArr.js
@@ -0,0 +1,6 @@
+
+// Problem 13: Remove Duplicates from Array  [Easy]
+// Description: Write a function removeDuplicates(arr) that returns a new array with duplicate values removed.
+// Example:
+// Input: [1, 2, 2, 3, 3, 4]  → Output: [1, 2, 3, 4]
+// Hint: Use Set or filter() with indexOf().
