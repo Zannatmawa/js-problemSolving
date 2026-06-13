e14bae94d4a96e40b7cd106f56d7506fa7ed4273
zannatul mawa <zannatulmawa38@gmail.com>
2026-06-13 15:48:24 +0600

    solving day 5 problems

---
+++ day5/memoizeFunc.js
@@ -0,0 +1,6 @@
+
+// Problem 25: Memoize a Function  [Medium]
+// Description: Write a function memoize(fn) that caches the results of a function so repeated calls with the same input return the cached result.
+// Example:
+// const memoAdd = memoize(n => n + 10);memoAdd(5); // 15 (computed)memoAdd(5); // 15 (from cache)
+// Hint: Use an object as a cache inside the outer function.
