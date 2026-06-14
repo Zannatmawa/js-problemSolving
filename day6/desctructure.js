c95075bee43651f92ce333f6f5672edc09bc441e
zannatul mawa <zannatulmawa38@gmail.com>
2026-06-14 16:55:57 +0600

    solving day 6 problem

--- day6/desctructure.js
+++ day6/desctructure.js
@@ -1,8 +1,46 @@
 // Day 6 — ES6+ Features
- 
+
 // Problem 26: Destructure and Rename  [Easy]
 // Description: Given a user object, use destructuring to extract firstName, lastName, and age, renaming firstName to name.
 // Example:
 // const user = {firstName:'Sara', lastName:'Khan', age:25};// Extract: name='Sara', lastName='Khan', age=25
 // Hint: Use: const { firstName: name, lastName, age } = user;
+
+const user = {
+    firstName: 'Sara',
+    lastName: 'Khan',
+    age: 25
+};
+
+const { firstName: n, lastn, age } = user;
+
+console.log(name);
+console.log(lastn);
+console.log(age);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
