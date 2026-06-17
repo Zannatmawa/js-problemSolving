430828cf2478635014b4b46fc2212badb2159928
zannatul mawa <zannatulmawa38@gmail.com>
2026-06-17 09:49:31 +0600

    solving day 8  problem

--- day7/debounceFunc.js
+++ day7/debounceFunc.js
@@ -5,6 +5,7 @@
 // Example:
 // const debouncedSearch = debounce(search, 300);// Fires only once after user stops typing for 300ms
 // Hint: Use clearTimeout and setTimeout.
+//git add . && git commit -m "solving day 7 problem" && git push
 
 
 
---
+++ day8/safeJson.js
@@ -0,0 +1,34 @@
+// Day 8 — Error Handling & Advanced Patterns
+ 
+// Problem 36: Safe JSON Parse[Easy]
+// Description: Write a function safeJsonParse(str) that tries to parse a JSON string and returns the parsed value, or null if parsing fails — without throwing an error.
+//     Example:
+// safeJsonParse('{"a":1}');  // {a: 1}safeJsonParse('bad json'); // null
+// Hint: Use try/catch inside the function.
+
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
