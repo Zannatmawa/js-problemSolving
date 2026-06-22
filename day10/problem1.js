76070cc00b4872ab00932ae6bb17d6eb7851f0fe
zannatul mawa <zannatulmawa38@gmail.com>
2026-06-22 21:58:18 +0600

    solved longest sentence problem

--- day10/problem1.js
+++ day10/problem1.js
@@ -1,34 +1,41 @@
 // Day 10 — Algorithms & Problem Solving II
- 
+
 // Problem 46: Count Words in a Sentence  [Easy]
 // Description: Write a function wordCount(sentence) that returns an object with each word as a key and its frequency as the value.
 // Example:
 // Input: 'the cat sat on the mat'Output: {the: 2, cat: 1, sat: 1, on: 1, mat: 1}
 // Hint: Split by spaces, then reduce into a frequency object.
 
- 
- 
- 
- 
- 
- 
-
- 
- 
- 
- 
- 
+let count = 0
+function wordCount(sentence) {
+    let sent = sentence.split(" ")
+    for (const element of sent) {
+        console.log(element)
+    }
+    return sent
+}
+console.log(wordCount('the cat sat on the mat'))
 
 
- 
- 
- 
- 
- 
 
- 
- 
- 
- 
- 
- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- day10/problem2.js
+++ day10/problem2.js
@@ -1,6 +1,20 @@
 // Problem 47: Longest Word in a Sentence  [Easy]
-// Description: Write a function longestWord(sentence) that returns the longest word in a sentence. If there's a tie, return the first one.
+// Description: Write a function longestWord(sentence) that returns the longest word in a sentence. 
+// If there's a tie, return the first one.
 // Example:
 // Input: 'The quick brown fox'Output: 'quick'
 // Hint: Split the sentence and use reduce() to track the longest.
+function longestWord(sentence) {
+    let words = sentence.split(" ");
+    let longest = words[0];
 
+    for (let i = 1; i < words.length; i++) {
+        if (words[i].length > longest.length) {
+            longest = words[i];
+        }
+    }
+
+    return longest;
+}
+
+console.log(longestWord('The quick brown fox')); // quick
