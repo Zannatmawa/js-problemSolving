77e2f3876a99da9c65fbd9e9d1b56e90bc861815
zannatul mawa <zannatulmawa38@gmail.com>
2026-06-24 21:57:26 +0600

    feat: implement recursive function to flatten nested objects using dot-notation keys

--- day8/flattenObj.js
+++ day8/flattenObj.js
@@ -3,3 +3,22 @@
 //     Example:
 // Input: { a: { b: { c: 1 } } } Output: { 'a.b.c': 1 }
 // Hint: Use recursion; build the key by joining parent keys with dots.
+function flattenObject(obj, parentKey = "", result = {}) {
+    for (let key in obj) {
+        const newKey = parentKey
+            ? `${parentKey}.${key}`
+            : key;
+
+        if (
+            typeof obj[key] === "object" &&
+            obj[key] !== null &&
+            !Array.isArray(obj[key])
+        ) {
+            flattenObject(obj[key], newKey, result);
+        } else {
+            result[newKey] = obj[key];
+        }
+    }
+
+    return result;
+}
