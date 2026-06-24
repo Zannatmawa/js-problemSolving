e11867e65312a9e104117ee2e0c480cb6b7e083c
zannatul mawa <zannatulmawa38@gmail.com>
2026-06-24 21:55:23 +0600

    feat: implement custom Array.prototype.map function from scratch

--- day7/eventEmitter.js
+++ day7/eventEmitter.js
@@ -3,3 +3,32 @@
 //     Example:
 // const emitter = new EventEmitter(); emitter.on('greet', name => console.log('Hello ' + name)); emitter.emit('greet', 'Sara'); // Hello Sara
 // Hint: Store listeners in an object where keys are event names and values are arrays of functions.
+class EventEmitter {
+    constructor() {
+        this.events = {};
+    }
+
+    on(event, listener) {
+        if (!this.events[event]) {
+            this.events[event] = [];
+        }
+
+        this.events[event].push(listener);
+    }
+
+    emit(event, ...args) {
+        if (!this.events[event]) return;
+
+        this.events[event].forEach(listener => {
+            listener(...args);
+        });
+    }
+
+    off(event, listenerToRemove) {
+        if (!this.events[event]) return;
+
+        this.events[event] = this.events[event].filter(
+            listener => listener !== listenerToRemove
+        );
+    }
+}
