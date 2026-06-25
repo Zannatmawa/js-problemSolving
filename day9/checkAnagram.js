819867ca1c57bb1123450b23fe45345c3209d0be
zannatul mawa <zannatulmawa38@gmail.com>
2026-06-25 21:39:11 +0600

    problem solving

--- day9/checkAnagram.js
+++ day9/checkAnagram.js
@@ -4,9 +4,13 @@
 // Input: 'listen', 'silent'  → Output: trueInput: 'hello', 'world'   → Output: false
 // Hint: Sort both strings and compare, or use a character frequency map.
 function isAnagram(str1, str2) {
-    if (str1 === "") {
-
-    } else {
-
+    if (str1.length !== str2.length) {
+        return false;
     }
+
+    return str1.split("").sort().join("") ===
+        str2.split("").sort().join("");
 }
+
+console.log(isAnagram("listen", "silent"));
+console.log(isAnagram("hello", "world"));   
