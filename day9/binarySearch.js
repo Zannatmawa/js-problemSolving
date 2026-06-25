2808c2874ee336ce3b52feb08825c4bb60aecb91
zannatul mawa <zannatulmawa38@gmail.com>
2026-06-25 21:38:38 +0600

    problem solving

--- day9/binarySearch.js
+++ day9/binarySearch.js
@@ -3,3 +3,24 @@
 //     Example:
 // Input: [1, 3, 5, 7, 9], target = 7  → Output: 3
 // Hint: Use left and right pointers; check the middle element each iteration.
+
+function binarySearch(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (arr[mid] === target) {
+            return mid;
+        }
+
+        if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+}
