557e8de5602f34070f2496398c5bbe5aaf3bc433
zannatul mawa <zannatulmawa38@gmail.com>
2026-06-26 22:41:06 +0600

    solving problem3

--- day10/problem4.js
+++ day10/problem4.js
@@ -4,3 +4,31 @@
 // Example:
 // Input: 'III'   → Output: 3Input: 'IX'    → Output: 9Input: 'LVIII' → Output: 58
 // Hint: Map each symbol to its value; if a smaller value comes before a larger one, subtract it.
+function romanToInt(s) {
+    const roman = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    };
+
+    let total = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        if (roman[s[i]] < roman[s[i + 1]]) {
+            total -= roman[s[i]];
+        } else {
+            total += roman[s[i]];
+        }
+    }
+
+    return total;
+}
+
+console.log(romanToInt("III"));    // 3
+console.log(romanToInt("IX"));     // 9
+console.log(romanToInt("LVIII"));  // 58
+console.log(romanToInt("MCMXCIV"));// 1994
