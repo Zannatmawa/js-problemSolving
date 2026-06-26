af5154290d45ab222c01abf2ff6b4e33a1417443
zannatul mawa <zannatulmawa38@gmail.com>
2026-06-26 22:41:27 +0600

    solving problem3

--- day10/problem5.js
+++ day10/problem5.js
@@ -3,3 +3,23 @@
 // Example:
 // Input: 0  → Output: [1]Input: 3  → Output: [1,3,3,1]Input: 4  → Output: [1,4,6,4,1]
 // Hint: Each element is the sum of the two elements above it from the previous row.
+function pascalRow(n) {
+    let row = [1];
+
+    for (let i = 1; i <= n; i++) {
+        const newRow = [1];
+
+        for (let j = 1; j < i; j++) {
+            newRow.push(row[j - 1] + row[j]);
+        }
+
+        newRow.push(1);
+        row = newRow;
+    }
+
+    return row;
+}
+
+console.log(pascalRow(0)); // [1]
+console.log(pascalRow(3)); // [1, 3, 3, 1]
+console.log(pascalRow(4)); // [1, 4, 6, 4, 1]
